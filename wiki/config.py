5c7d5c97c635b6a06f44ec68ef5ccbbb14868417
Ibrahimalshahrani <skym0on@hotmail.com>
2017-10-15 03:15:11 -0400

    Add files via upload

---
+++ wiki/config.py
@@ -0,0 +1,8 @@
+# encoding: utf-8
+
+SECRET_KEY='a unique and long key'
+TITLE='Riki' 
+HISTORY_SHOW_MAX=30
+PIC_BASE = '/static/content/'
+CONTENT_DIR = '/Users/smcho/Desktop/wiki-master/content/'
+NUMBER_OF_HISTORY = 5
